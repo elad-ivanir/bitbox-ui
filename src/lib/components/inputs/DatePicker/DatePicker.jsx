9195a834b110077c3240380365bf6c0eb034246c
elad-ivanir <eladivanir64@gmail.com>
2020-04-01 17:14:53 +0300

    making boilerplates for controls

---
+++ src/lib/components/inputs/DatePicker/DatePicker.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import textInputPropTypes from "../../../propTypes/textInputPropTypes";
+
+const DatePicker = ({ id, value, onChange }) => null;
+
+DatePicker.propTypes = { ...textInputPropTypes };
+
+export default DatePicker;
---
+++ src/lib/components/inputs/MultipleSelectDropDown/MultipleSelectDropDown.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import multipleSelectPropTypes from "../../../propTypes/multipleSelectPropTypes";
+
+const MultipleSelectDropDown = ({ id, onChange, options, selectedValues }) =>
+  null;
+
+MultipleSelectDropDown.propTypes = { ...multipleSelectPropTypes };
+
+export default MultipleSelectDropDown;
