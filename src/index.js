3370b9986708b52b54879976270e81c5a395db34
elad-ivanir <eladivanir64@gmail.com>
2020-04-27 23:53:49 +0300

    test in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ThemeProvider from "./lib/components/contextProviders/ThemeProvider/ThemeProvider";
 import CheckBox from "./lib/components/elements/CheckBox/CheckBox";
 
-const jsx = <CheckBox />;
+const theme = {
+  colors: {
+    main: "red"
+  },
+  typography: {
+    fontFamily: "Hack"
+  }
+};
+
+const jsx = (
+  <ThemeProvider theme={theme}>
+    <CheckBox text="hello" />
+  </ThemeProvider>
+);
 
 ReactDOM.render(jsx, document.getElementById("root"));
